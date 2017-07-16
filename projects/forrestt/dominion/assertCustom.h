3f2b9da36b8c087be51f433d5ac4fec49d18985b
tylerforrester <forrestt@oregonstate.edu>
2017-07-15 18:29:36 -0600

    added helper functions

--- projects/forrestt/dominion/assertCustom.h
+++ projects/forrestt/dominion/assertCustom.h
@@ -5,4 +5,8 @@
 #ifndef DOMINION_ASSERTCUSTOM_H
 #define DOMINION_ASSERTCUSTOM_H
 
+void assertCustom(int boolean, char * message);
+
+void assertGameState(int player, struct gameState * oldG, struct gameState * newG);
+
 #endif //DOMINION_ASSERTCUSTOM_H
